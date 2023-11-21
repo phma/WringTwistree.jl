62e21d298d47c3e728fd61ff6e64fd72679d2bcf
Pierre Abbat <phma@bezitopo.org>
2023-11-21 08:16:46 +0000

    Move mix3 code to new Mix3 module

---
+++ src/Mix3.jl
@@ -0,0 +1,34 @@
+module Mix3
+export fiboPair,mix3Parts!
+
+function mix3(a::Integer,b::Integer,c::Integer)
+  mask=(a|b|c)-(a&b&c)
+  (a⊻mask,b⊻mask,c⊻mask)
+end
+
+function fiboPair(n::Integer)
+  f0=0
+  f1=1
+  while f0<=n
+    (f0,f1)=(f1,f1+f0)
+  end
+  (f0,f1)
+end
+
+function mix3Parts!(buf::Vector{<:Integer},rprime::Integer)
+  len=div(length(buf),3)
+  a=1
+  b=2*len
+  c=2*len+1
+  while len>0 && a<b
+    (buf[a],buf[b],buf[c])=mix3(buf[a],buf[b],buf[c])
+    a+=1
+    b-=1
+    c+=rprime
+    if c>3*len
+      c-=len
+    end
+  end
+end
+
+end
