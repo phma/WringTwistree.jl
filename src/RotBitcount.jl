0deda83b37a2a6633ae3b84a079d744eaa26405f
Pierre Abbat <phma@bezitopo.org>
2023-11-24 05:42:29 +0000

    Comment RotBitcount module

--- src/RotBitcount.jl
+++ src/RotBitcount.jl
@@ -1,6 +1,14 @@
 module RotBitcount
 export rotBitcount!
 
+# This module is used in both Wring and Twistree.
+# It rotates an array of bytes by a multiple of its bitcount,
+# producing another array of the same size. As long as the multiplier
+# is relatively prime to the number of bits in the array, this
+# operation satisfies the strict avalanche criterion. Changing *two*
+# bits, however, has half a chance of changing only two bits in
+# the output.
+
 function rotBitcount!(src::Vector{UInt8},dst::Vector{UInt8},mult::Integer)
   len=length(src)
   if len!=length(dst)
@@ -14,7 +22,7 @@ function rotBitcount!(src::Vector{UInt8},dst::Vector{UInt8},mult::Integer)
   rotcount=(bitcount*multmod)%(len*8)
   byte=rotcount>>3
   bit=rotcount&7
-  for i in 1:length(dst)
+  for i in 1:len
     dst[i]=(src[(i+len-byte-1)%len+1]<<bit) | (src[(i+len-byte-2)%len+1]>>(8-bit))
   end
 end
