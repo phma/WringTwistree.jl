8380e248c05fd7bf7010616fe12a7b941ffc75b7
Pierre Abbat <phma@bezitopo.org>
2023-11-30 09:50:35 +0000

    Make a keyed Wring

--- src/WringTwistree.jl
+++ src/WringTwistree.jl
@@ -2,9 +2,22 @@ module WringTwistree
 include("Mix3.jl")
 include("RotBitcount.jl")
 include("Sboxes.jl")
+using OffsetArrays
 using .Mix3,.RotBitcount,.Sboxes
 export carmichael,sboxes,inverse
+export keyedWring
 # carmichael is exported in case someone wants the Carmichael function,
 # which I couldn't find.
+
+struct Wring
+  sbox    ::OffsetArray{UInt8}
+  invSbox ::OffsetArray{UInt8}
+end
+
+function keyedWring(key) # key is a String or Vector{UInt8}
+  sbox=sboxes(key)
+  invSbox=inverse(sbox)
+  Wring(sbox,invSbox)
+end
 
 end # module WringTwistree
