255d918f31465b700e9e4dfd824cb4b4be1638ad
Pierre Abbat <phma@bezitopo.org>
2023-12-05 06:49:02 +0000

    Define the lfsr table

--- src/Compress.jl
+++ src/Compress.jl
@@ -4,11 +4,22 @@ include("RotBitcount.jl")
 include("Sboxes.jl")
 using OffsetArrays
 using .Mix3,.RotBitcount,.Sboxes
-export blockSize,twistPrime,relPrimes
+export blockSize,twistPrime,relPrimes,lfsr
 
 const blockSize=32
 const twistPrime=37
 
 relPrimes=OffsetArray(map(findMaxOrder∘(x -> x÷0x3),collect(0x0020:0x0060)),0x20:0x60)
+
+function lfsr1(n::Integer)
+  ((n&1)*0x84802140)⊻(n>>1)
+end
+
+lfsr=OffsetArray(map(collect(0:255)) do x
+  for i in 1:8
+    x=lfsr1(x)
+  end
+  x
+end,0:255)
 
 end
