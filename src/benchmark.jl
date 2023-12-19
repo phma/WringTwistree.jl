2387cb3c3d3f8b5331979b641f68893783e8a599
Pierre Abbat <phma@bezitopo.org>
2023-12-19 06:58:33 +0000

    Compute the break-even point for Wring

--- src/benchmark.jl
+++ src/benchmark.jl
@@ -1,6 +1,7 @@
 
 key96 = "Водворетраванатраведрова.Нерубидрованатраведвора!"
 twistree96 = keyedTwistree(key96)
+wring96 = keyedWring(key96)
 text59049 = map(WringTwistree.xorn,collect(1:59049))
 
 hash!(twistree96,text59049);
@@ -21,6 +22,34 @@ function twistreeBreakEven()
   while maxLength-minLength>1
     midLength=(minLength+maxLength)÷2
     midRatio=twistreeTimeRatio(midLength)
+    println("Length=",midLength," Ratio=",midRatio)
+    if midRatio>1
+      maxLength=midLength
+      maxRatio=midRatio
+    else
+      minLength=midLength
+      minRatio=midRatio
+    end
+  end
+  midLength
+end
+
+function wringTimeRatio(n::Integer)
+  textn=map(WringTwistree.xorn,collect(1:n))
+  trialSeq=@benchmark encrypt!(wring96,$textn,:sequential)
+  trialPar=@benchmark decrypt!(wring96,$textn,:parallel)
+  median(trialSeq).time/median(trialPar).time
+end
+
+function wringBreakEven()
+  minLength=1
+  minRatio=wringTimeRatio(minLength) # <1
+  maxLength=100000
+  maxRatio=wringTimeRatio(maxLength) # >1
+  midLength=0
+  while maxLength-minLength>1
+    midLength=(minLength+maxLength)÷2
+    midRatio=wringTimeRatio(midLength)
     println("Length=",midLength," Ratio=",midRatio)
     if midRatio>1
       maxLength=midLength
